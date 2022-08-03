93da0fa9298cac32edec3c45de0c2942ec08c2a0
Maxime VALENCIK <89088940+maxvalencik@users.noreply.github.com>
2022-08-02 19:20:58 -0700

    final

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ from forms import RegisterForm, LoginForm, FeedbackForm, DeleteForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///flask-feedback"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///flask_feedback"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = "shhhhh"
+app.config['SECRET_KEY'] = "vansusopen"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
--- models.py
+++ models.py
@@ -27,53 +27,53 @@ class User(db.Model):
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
 
-#     feedback = db.relationship(
-#         "Feedback", backref="user", cascade="all,delete")
+    feedback = db.relationship(
+        "Feedback", backref="user", cascade="all,delete")
 
-#     # start of convenience class methods
+    # start of convenience class methods
 
-#     @classmethod
-#     def register(cls, username, password, first_name, last_name, email):
-#         """Register a user, hashing their password."""
+    @classmethod
+    def register(cls, username, password, first_name, last_name, email):
+        """Register a user, hashing their password."""
 
-#         hashed = bcrypt.generate_password_hash(password)
-#         hashed_utf8 = hashed.decode("utf8")
-#         user = cls(
-#             username=username,
-#             password=hashed_utf8,
-#             first_name=first_name,
-#             last_name=last_name,
-#             email=email
-#         )
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+        user = cls(
+            username=username,
+            password=hashed_utf8,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
 
-#         db.session.add(user)
-#         return user
+        db.session.add(user)
+        return user
 
-#     @classmethod
-#     def authenticate(cls, username, password):
-#         """Validate that user exists & password is correct.
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validate that user exists & password is correct.
 
-#         Return user if valid; else return False.
-#         """
+        Return user if valid; else return False.
+        """
 
-#         user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first()
 
-#         if user and bcrypt.check_password_hash(user.password, password):
-#             return user
-#         else:
-#             return False
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        else:
+            return False
 
 
-# class Feedback(db.Model):
-#     """Feedback."""
+class Feedback(db.Model):
+    """Feedback."""
 
-#     __tablename__ = "feedback"
+    __tablename__ = "feedback"
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(100), nullable=False)
-#     content = db.Column(db.Text, nullable=False)
-#     username = db.Column(
-#         db.String(20),
-#         db.ForeignKey('users.username'),
-#         nullable=False,
-#     )
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(
+        db.String(20),
+        db.ForeignKey('users.username'),
+        nullable=False,
+    )
